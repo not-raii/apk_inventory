10b33b2fb611e8334477c9bee5e3837ac4b9c275
not-raii <raifadhilah85@gmail.com>
2023-06-11 19:00:24 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::get('/login', function (){
+    return view('login');
 });
