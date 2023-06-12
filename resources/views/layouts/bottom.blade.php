9f83f6402f10be62c667c60e702f041aaeada9c6
not-raii <raifadhilah85@gmail.com>
2023-06-12 17:13:09 +0700

    blading

---
+++ resources/views/layouts/bottom.blade.php
@@ -0,0 +1,14 @@
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ '/assets/vendor/jquery/jquery.min.js' }}"></script>
+    <script src="{{ '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js' }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ '/assets/vendor/jquery-easing/jquery.easing.min.js' }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ '/assets/js/script.js' }}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{ '/assets/vendor/chart.js/Chart.min.js' }}"></script>
+
+
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,1 +1,105 @@
-        
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon rotate-n-15">
+        <i class="fas fa-laugh-wink"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">Gudang-IT</div>
+</a>
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item active">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Master Data
+</div>
+
+<!-- Nav Item - Pages Collapse Menu Users -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePageUser"
+        aria-expanded="true" aria-controls="collapsePageUser">
+        <i class="fas fa-fw fa-folder"></i>
+        <span>Data Pengguna</span>
+    </a>
+    <div id="collapsePageUser" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="register.html">Admin</a>
+            <a class="collapse-item" href="forgot-password.html">User</a>
+        </div>
+    </div>
+</li>
+
+
+<!-- Nav Item - Kategori -->
+<li class="nav-item">
+    <a class="nav-link" href="tables.html">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Kategori</span></a>
+</li>
+
+<!-- Nav Item - Barang -->
+<li class="nav-item">
+    <a class="nav-link" href="tables.html">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Barang</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Data Laporan
+</div>
+
+<!-- Nav Item - Barang Masuk -->
+<li class="nav-item">
+    <a class="nav-link" href="tables.html">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Barang Masuk</span></a>
+</li>
+
+<!-- Nav Item - Barang Keluar -->
+<li class="nav-item">
+    <a class="nav-link" href="tables.html">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Barang Keluar</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Pengaturan
+</div>
+
+<!-- Nav Item - Manajemen Users -->
+<li class="nav-item">
+    <a class="nav-link" href="tables.html">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Manajemen Pengguna</span></a>
+</li>
+
+<!-- Nav Item - Log Out Menu -->
+<li class="nav-item">
+    <a class="nav-link" href="tables.html">
+    <i class="fas fa-sign-out-alt fa-sm fa-fw"></i>
+        <span>Keluar</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
