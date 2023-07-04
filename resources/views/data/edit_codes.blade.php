080ece43d65c534ad5a2bc287929cc40dd7c50d8
not-raii <raifadhilah85@gmail.com>
2023-07-04 10:52:09 +0700

    filter

---
+++ resources/views/data/edit_codes.blade.php
@@ -0,0 +1,56 @@
+@extends('templates/default')
+
+@section('content')
+<!-- Page Heading -->
+<div class="page-heading d-flex justify-content-between mx-4 mb-2">
+    <h1 class="h3 mb-2 text-gray-800">Kode Barang</h1>
+</div>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Edit Kode Barang</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <form action="kode_barang" method="POST" enctype="multipart/form-data">
+                @method('PUT')
+                @csrf
+                <div class="form-group">
+                    <label for="kode">Kode Barang</label>
+                    <input name="kode_barang" type="text"
+                        class="form-control @error('kode_barang') is-invalid @enderror" id="kode" placeholder="ABCDE"
+                        value="{{ $kode }}">
+                </div>
+                <div class="form-group">
+                    <label for="jumlah">Gambar Barang</label>
+                    <input name="gambar_barang" type="file"
+                        class="form-control @error('gambar_barang') is-invalid @enderror" id="gambar" value="{{  }}">
+                </div>
+                <div class="form-group">
+                    <label for="nama">Nama Barang</label>
+                    <input name="nama_barang" type="text"
+                        class="form-control @error('nama_barang') is-invalid @enderror" id="nama" placeholder="ASUS"
+                        value="{{ old('nama_barang') }}">
+                </div>
+                <div class="form-group">
+                    <label for="kategori">Kategori</label>
+                    <select name="id_categories" class="form-control" id="kategori">
+                        <option value="">Pilih Kategori</option>
+                        @foreach ($kategori as $item)
+                        <option value="{{ $item->id }}">{{ $item->nama_kategori }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="d-flex justify-content-between mx-2">
+                    <a class="btn btn-info" href="kode_barang"> Kembali </a>
+                    <button class="btn btn-success" href="kode_barang_add" type="submit">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+</div>
+<!-- /.container-fluid -->
+@endsection
